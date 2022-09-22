c242ac093fbb1a363361dd7f2a7435e69671b97f
Shaya <suarezshaya@gmail.com>
2022-09-22 17:12:40 -0300

    Desafio 6

--- Routes/products.js
+++ Routes/products.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { Router } = express;
 const router = Router();
-const Contenedor = require("../clase4/desafio-clase4");
+const Contenedor = require("./contenedor");
 const constructor = new Contenedor("./productos.json");
 
 router.get("/", (req, res) => {
   try {
-    res.send(constructor.getAll());
+    res.send(Contenedor.getAll());
   } catch (err) {
     res.status(404).send(err);
   }
@@ -15,7 +15,7 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   try {
     const { id } = req.params;
-    res.send(constructor.getById(parseInt(id)));
+    res.send(Contenedor.getById(parseInt(id)));
   } catch (err) {
     res.status(404).send(err);
   }
@@ -24,8 +24,8 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   try {
     const data = req.body;
-    const ID = constructor.save(data);
-    res.send({ ID });
+    Contenedor.save(data);
+    res.redirect("/");
   } catch (err) {
     res.status(404).send(err);
   }
@@ -36,7 +36,7 @@ router.put("/:id", (req, res) => {
     const { id } = req.params;
     const prodNuevo = req.body;
     const idInt = parseInt(id);
-    res.send(constructor.updateById(idInt, prodNuevo));
+    res.send(Contenedor.updateById(idInt, prodNuevo));
   } catch (err) {
     res.status(404).send(err.msg);
   }
@@ -45,7 +45,7 @@ router.put("/:id", (req, res) => {
 router.delete("/:id", (req, res) => {
   try {
     const { id } = req.params;
-    res.send(constructor.deleteById(parseInt(id)));
+    res.send(Contenedor.deleteById(parseInt(id)));
   } catch (err) {
     res.status(404).send(err.msg);
   }
